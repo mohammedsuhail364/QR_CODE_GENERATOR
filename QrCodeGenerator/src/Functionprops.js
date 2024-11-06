5d143034fdce1cb177dd7029eea2260214a4eeea
mohammedsuhail364 <mohammedthanish364@gmail.com>
2024-11-06 15:57:42 +0530

    Updates

--- QrCodeGenerator/src/Functionprops.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-function Functionprops(props){
-    return(
-        <div><h3>This is functional component</h3>,
-        <h3>hello {props.name}from {props.place}</h3>
-        </div>
-    );
-}
-export default Functionprops;
--- QrCodeGenerator/src/component/functioncomponent.js
+++
@@ -1,7 +0,0 @@
-import React from "react"
-
-function FunctionalComp() {
-    return <h1>This is a functional component</h1>
-}
-
-export default FunctionalComp;
